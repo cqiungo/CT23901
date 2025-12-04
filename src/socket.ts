d155905b3473e246c8029d2dd73f34b3d2ebff96
cqiungo <okihawa4@gmail.com>
2025-12-04 23:29:38 +0700

    vercel fix

--- src/socket.ts
+++ src/socket.ts
@@ -3,5 +3,7 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://ct239-server01.onrender.com';
 
-export const socket = io(URL);
+export const socket = io(URL,{
+  transports: ["websocket"],
+});
 
